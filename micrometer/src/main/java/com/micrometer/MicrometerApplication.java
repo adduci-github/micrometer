8ec2632ee3b661a5b71004a642b7c5c59699ac38
AzureAD\정윤종 <yjjung@jellypage.com>
2023-03-23 15:14:04 +0900

    a

--- micrometer/src/main/java/com/micrometer/MicrometerApplication.java
+++ micrometer/src/main/java/com/micrometer/MicrometerApplication.java
@@ -4,10 +4,12 @@ import io.micrometer.observation.ObservationTextPublisher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class MicrometerApplication {
     public static void main(String[] args) {
+        Hooks.enableAutomaticContextPropagation();
         SpringApplication.run(MicrometerApplication.class, args);
     }
 
--- micrometer/src/main/java/com/micrometer/greeting/controller/GreetingController.java
+++ micrometer/src/main/java/com/micrometer/greeting/controller/GreetingController.java
@@ -2,21 +2,24 @@ package com.micrometer.greeting.controller;
 
 import com.micrometer.greeting.record.Greeting;
 import com.micrometer.greeting.service.GreetingService;
+import io.micrometer.observation.ObservationRegistry;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.observability.micrometer.Micrometer;
 import reactor.core.publisher.Mono;
 
 @RestController
+@RequiredArgsConstructor
 public class GreetingController {
     private final GreetingService service;
-
-    GreetingController(GreetingService service) {
-        this.service = service;
-    }
+    private final ObservationRegistry registry;
 
     @GetMapping("/hello/{name}")
     public Mono<Greeting> greeting(@PathVariable("name") String name) {
-        return service.greeting(name);
+        return service.greeting(name)
+                .name("greeting.call")
+                .tap(Micrometer.observation(registry));
     }
 }
